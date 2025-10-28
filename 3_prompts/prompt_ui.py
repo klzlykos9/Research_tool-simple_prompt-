06d52a474c36ce3440afe1b7e0b000e932c05a6a
klzlykos9 <a.klauszzz3@gmail.com>
2025-10-28 07:56:58 +0530

    2nd commit

--- 3_prompts/prompt_ui.py
+++ 3_prompts/prompt_ui.py
@@ -20,24 +20,7 @@ style_input = st.selectbox("Select Explanation Style", ['Beginner-Friendly', 'Te
 
 length_input = st.selectbox("Select Explanation length", ['Short (1-2 paragraphs)', 'Medium (3-5 paragraphs)', 'Long (detailed explanation)'])
 
-# template
-template = PromptTemplate(
-    template = 
-"""Please summarize the research paper titled {paper_input} with the following
-specifications:
-Explanation style: {style_input}
-Explanation length: {length_input}
-1. Mathematical Details:
-    - Include relevant mathematical equations if present in the paper.
-    - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-2. Analogies :-
-    - Use relatable analogies to simply complex ideas.
-    If certain analogies is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-    Ensure the summary is clear, accurate and aligned with the provided style and length.
-
-""",
-input_variables=['paper_input', 'style_input', 'length_input']
-)
+template = load_prompt(r'D:\langchain practice\template.json')
 
 # fill the placeholders
 prompt = template.invoke({
